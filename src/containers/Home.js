3a025d0e8624cd13fa0543853fcb39b483fc0c54
github username <dirtyassweed@icloud.com>
2021-03-16 14:03:18 -0400

    adds banner component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../Home.css';
+import Banner from '../components/Banner';
 
 const Home = () => {
     return (
         <div className="home">
-            {/* <h1>home component</h1> */}
+            <Banner />
         </div>
     )
 }
