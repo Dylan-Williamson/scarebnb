f046136e78770beee2f78a3f8a31de9868eba8ee
github username <dirtyassweed@icloud.com>
2021-03-17 13:46:15 -0400

    adds searchPage component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
-import Home from './containers/Home';
 import Header from './components/Header';
+import Home from './containers/Home';
+import SearchPage from './components/SearchPage';
 import Footer from './components/Footer';
+import { Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
-      <Home />
+      <Switch>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Redirect from="*" to={"/"}/>
+      </Switch>
       <Footer />
     </div>
   );
